dc0de4c0c746d78368678575f4161fb24e367009
Chao Qu <quchao@seas.upenn.edu>
2014-02-26 23:04:09 -0500

    project3: add some util function

--- project3/code/PlotImuData.m
+++ project3/code/PlotImuData.m
@@ -1,6 +1,32 @@
-function h_fig = PlotImuData( imu, varargin )
+function h_fig = PlotImuData( imu, gesture, varargin )
 % PLOTIMUDATA visualize imu data
+plot_list = varargin;
+if nargin < 3, plot_list = {'acc', 'omg'}; end
+if nargin < 2, gesture = 'unknown'; end
 
-[t, acc, omg, mag] = SplitImuData(varargin{1});
+% Split imu data
+[t, acc, omg, mag] = SplitImuData( imu );
+
+% Plot imu data
+h_fig = figure('Name', gesture);
+num_plot = length(plot_list);
+for i = 1:num_plot
+    if strcmp(plot_list{i}, 'acc')
+        data = acc;
+        labels = {'ax', 'ay', 'az'};
+    elseif strcmp(plot_list{i}, 'omg')
+        data = omg;
+        labels = {'wx', 'wy', 'wz'};
+    elseif strcmp(plot_list{i}, 'mag')
+        data = mag;
+        labels = {'mx', 'my', 'mz'};
+    end
+    for j = 1:3
+        subplot(3, num_plot, num_plot*(j-1)+i)
+        plot(t, data(:,j))
+        title(labels{j})
+        xlim([t(1), t(end)])
+    end
+end
 
 end
--- project3/code/SplitImuData.m
+++ project3/code/SplitImuData.m
@@ -3,9 +3,9 @@ function [ t, acc, omg, mag ] = SplitImuData( imu )
 % and magnotometer readings
 assert(size(imu,2) == 10, 'Wrong imu dimension');
 
-t   = imu(1,:);
-acc = imu(2:4,:);
-omg = imu(5:7,:);
-mag = imu(8:10,:);
+t   = imu(:,1);
+acc = imu(:,2:4);
+omg = imu(:,5:7);
+mag = imu(:,8:10);
 
 end
