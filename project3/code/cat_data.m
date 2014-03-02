76b7a88fd169ed3d04807aa3054149eec6707390
Chao Qu <quchao@seas.upenn.edu>
2014-03-01 19:43:05 -0500

    proj3: formatting

--- project3/code/cat_data.m
+++ project3/code/cat_data.m
@@ -1,6 +1,6 @@
 function [ imu_all ] = cat_data( data, gesture, num_data )
 %CAT_ALL_DATA concatenate all imu data given gesture_list
-gesture_list = {'circle', 'figure8', 'fish', 'hammer', 'pend', 'wave'}; 
+gesture_list = {'circle', 'figure8', 'fish', 'hammer', 'pend', 'wave'};
 if nargin < 2, gesture = gesture_list; end
 if nargin < 3, num_data = length(data(1).data); end
 
