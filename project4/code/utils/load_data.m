839faa29f929655a4f7331c9b863ba9fbb4284a4
Chao Qu <quchao@seas.upenn.edu>
2014-03-07 20:55:19 -0500

    proj4: add some simple util functions

---
+++ project4/code/utils/load_data.m
@@ -0,0 +1,18 @@
+function [ data ] = load_data( data_id, data_path )
+%LOAD_DATA load sensor data according to data_id
+% [ data ] = load_data( data_id, data_path )
+
+if nargin < 2, data_path = '../data/'; end
+
+% imuRaw
+data.imu = load(fullfile(data_path, sprintf('imuRaw%d.mat', data_id)));
+
+% Hokuyo0
+lidar = load(fullfile(data_path, sprintf('Hokuyo%d.mat', data_id)));
+data.lidar = lidar.Hokuyo0;
+
+% Encoders
+enc = load(fullfile(data_path, sprintf('Encoders%d.mat', data_id)));
+data.enc = enc.Encoders;
+
+end
