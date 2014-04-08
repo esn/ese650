d17f368ae13ad7f81792293429dd06c2f409bbce
Chao Qu <quchao@seas.upenn.edu>
2014-04-08 12:34:21 -0400

    proj5: add demo code

--- project5/code/proj5_main.m
+++ project5/code/proj5_main.m
@@ -6,18 +6,20 @@ mat_name = 'mat/data.mat';
 rgb_name = 'mat/rgb.mat';
 load(mat_name);
 load(rgb_name);
+type = 'walk';
 
 %% Instantiate MDP for diver
 n = 1:2:numel(sub);
-type = 'drive';
 for i = 1:numel(n)
     mdp(i) = MDP(sub{n(i)}, type);
     mdp(i).addPolicy();
     mdp(i).plot();
 end
-mdp(i+1) = MDP(im_rgb(1000:2000,1000:2500,:), type);
+mdp(i+1) = MDP(im_rgb(500:1500,1000:2500,:), type);
 mdp(i+1).addPolicy();
-save(['mat/mdp_' type], 'mdp')
+% save(['mat/mdp_' type], 'mdp')
+%%
+clear mdp
 load(['mat/mdp_' type])
 
 %% Instantiate LEARCH
@@ -25,6 +27,15 @@ T = 25;
 m = floor(0.6*numel(mdp));
 learch = LEARCH(mdp(1:m-1), mdp(m:end-1), T);
 learch.train(true);
+% save(['mat/learch_' type], 'learch')
+
+%%
+load(['mat/learch_', type]);
+
 learch.test(mdp(end));
 learch.test();
-save(['mat/learch_' type], 'learch')
+
+%%
+mdp(end).removePolicy();
+mdp(end).addPolicy();
+learch.test(mdp(end));
