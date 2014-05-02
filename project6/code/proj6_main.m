540cc7b2d8284272e0c87b4cd03c32237670a445
Chao Qu <quchao@seas.upenn.edu>
2014-05-01 20:39:09 -0400

    update

--- project6/code/proj6_main.m
+++ project6/code/proj6_main.m
@@ -13,12 +13,12 @@ gslam.plot();
 xlabel('x [m]')
 ylabel('y [m]')
 beautify(h_slam)
-title('Raw Pose Nodes')
+title('Raw Pose Nodes with Loop Closure')
 axis([-10 75 -35 15])
 set(gcf, 'Position', [100 100 800 500])
 
 %% Optimize
-gslam.optimize(15); % Do some number of iterations
+gslam.optimize(2); % Do some number of iterations
 
 %% Visualize
 h_slam = figure();
